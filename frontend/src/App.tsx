30d56751664af00a8844c73776a249d13bda956c
Horlarhyinka <danijufarouq2003@gmail.com>
2024-11-04 09:26:21 +0100

    added login screen

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,14 +2,14 @@ import { ChakraProvider } from "@chakra-ui/react";
 import "./App.css";
 // import HomePage from "./app/home";
 import theme from "./theme";
-import Login from "./app/login";
+import Register from "./app/login";
 
 function App() {
 
   return (
     <ChakraProvider theme={theme} >
       {/* <HomePage/> */}
-      <Login />
+      <Register />
     </ChakraProvider>
     
   );
